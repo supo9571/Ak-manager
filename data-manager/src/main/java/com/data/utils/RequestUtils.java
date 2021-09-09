2c481e7b56715cb58862ab3ba8320465eb0eaa23
lvbin <a315994685@163.com>
2021-09-09 20:18:06 +0800

    游客登录

--- data-manager/src/main/java/com/data/utils/RequestUtils.java
+++ data-manager/src/main/java/com/data/utils/RequestUtils.java
@@ -157,4 +157,17 @@ public class RequestUtils {
         redisTemplate.opsForValue().set(md5str, "1",1, TimeUnit.DAYS);
         return md5str;
     }
+
+    public String getRomodphone(){
+        String[] telFirst="134,135,136,137,138,139,150,151,152,157,158,159,130,131,132,155,156,133,153".split(",");
+        int index=getNum(0,telFirst.length-1);
+        String first=telFirst[index];
+        String second=String.valueOf(getNum(1,888)+10000).substring(1);
+        String third=String.valueOf(getNum(1,9100)+10000).substring(1);
+        return first+second+third;
+
+    }
+    public int getNum(int start,int end) {
+        return (int)(Math.random()*(end-start+1)+start);
+    }
 }
