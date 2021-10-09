084926aedad35154509aca48033d9412bbbe78a2
sieGuang <63412236426@qq.com>
2021-10-09 21:54:04 +0800

    修改  排序

--- data-manager/src/main/java/com/data/controller/AddUserController.java
+++ data-manager/src/main/java/com/data/controller/AddUserController.java
@@ -28,7 +28,6 @@ public class AddUserController extends BaseController {
      */
     @PostMapping("/list")
     public AjaxResult list(@RequestBody AddUser addUser) {
-        startOrder(addUser.getOrderByColumn(), addUser.getIsAsc());
         return AjaxResult.success("查询成功", addUserService.getList(addUser));
     }
 
