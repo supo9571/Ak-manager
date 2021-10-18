9e4f3efaded5623994a3e454b36a2e54dc19b13a
sieGuang <63412236426@qq.com>
2021-10-18 20:24:24 +0800

    直属玩家充值报表 平台id筛选

--- manager-common/src/main/java/com/manager/common/core/domain/model/DirectRecharge.java
+++ manager-common/src/main/java/com/manager/common/core/domain/model/DirectRecharge.java
@@ -14,6 +14,9 @@ import java.math.BigDecimal;
 @Data
 public class DirectRecharge extends BaseEntity {
 
+    @ApiModelProperty("平台id")
+    private String tid;
+
     @ApiModelProperty("玩家id")
     private String uid;
 
